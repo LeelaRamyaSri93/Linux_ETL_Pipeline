52ea355e4b4f673ade62e9877e6f68f913ffde9c
RamyaSri <ramyasrikaruturi93@gmail.com>
2025-09-12 14:14:56 +0000

    added database.sql file

---
+++ sql/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS drugs (
+    drug_name TEXT,
+    indications TEXT,
+    side_effects TEXT,
+    side_effect_severity TEXT,
+    drug_class TEXT,
+    approval_status TEXT
+);
